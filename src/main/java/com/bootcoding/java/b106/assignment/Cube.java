409f054d6152d24c9c6ea66a2c68275476ed0a54
Mrunnmayee-13A <mrunmayee13a@gmail.com>
2023-03-30 00:29:10 +0530

    Cube Example

---
+++ src/main/java/com/bootcoding/java/b106/assignment/Cube.java
@@ -0,0 +1,12 @@
+package com.bootcoding.java.b106.assignment;
+
+public class Cube {
+    public static void main(String[] args) {
+        int n = 5;
+        int cube;
+        for (int i = 1; i <= n; i++) {
+            cube = i * i * i;
+            System.out.println("Cube of a number = " + cube);
+        }
+    }
+}
