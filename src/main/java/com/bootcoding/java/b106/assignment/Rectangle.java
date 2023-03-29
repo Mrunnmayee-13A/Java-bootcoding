dc714edb58177116ded14cd48e8dd7a45c449a84
Mrunnmayee-13A <mrunmayee13a@gmail.com>
2023-03-29 23:57:03 +0530

    Rectangle Example

---
+++ src/main/java/com/bootcoding/java/b106/assignment/Rectangle.java
@@ -0,0 +1,12 @@
+package com.bootcoding.java.b106.assignment;
+
+public class Rectangle {
+    public static void main(String[] args) {
+         double width = 5.5;
+         double height = 8.5;
+         double peri = 2*(width + height);
+         System.out.println("Perimeter of rectangle = " +peri);
+         double ar = width * height;
+         System.out.println("Area of rectangle = " +ar);
+    }
+}
