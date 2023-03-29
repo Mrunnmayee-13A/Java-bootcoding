04dd7dcd1da0f57a9814cd20e6d7b1cfe9c0f7ec
Mrunnmayee-13A <mrunmayee13a@gmail.com>
2023-03-29 23:57:36 +0530

    Operation Example

---
+++ src/main/java/com/bootcoding/java/b106/assignment/Operations.java
@@ -0,0 +1,18 @@
+package com.bootcoding.java.b106.assignment;
+
+public class Operations {
+    public static void main(String[] args) {
+        int a=125;
+        int b=24;
+        int sum= a+b;
+        System.out.println("Sum of the numbers = " +sum);
+        int sub = a-b;
+        System.out.println("Subtraction of numbers = " +sub);
+        int multi = a*b;
+        System.out.println("Multiplication of numbers = " +multi);
+        float div = a/b;
+        System.out.println("Division of numbers = " +div);
+        int mod = a%b;
+        System.out.println("Modulo of the numbers = " +mod);
+    }
+}
