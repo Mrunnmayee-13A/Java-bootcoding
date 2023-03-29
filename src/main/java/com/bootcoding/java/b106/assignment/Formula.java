1d587b4e94c26bc0b28ba82d888e9baece603c29
Mrunnmayee-13A <mrunmayee13a@gmail.com>
2023-03-29 23:56:41 +0530

    Formula Example

---
+++ src/main/java/com/bootcoding/java/b106/assignment/Formula.java
@@ -0,0 +1,10 @@
+package com.bootcoding.java.b106.assignment;
+//4.0 * (1 - (1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11)) (edited)
+
+
+public class Formula {
+    public static void main(String[] args) {
+        double calculation = 4.0 * (1 - (1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11));
+        System.out.println("Formula's calculation=" +calculation);
+    }
+}
