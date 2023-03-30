d97600489c4fd2a81589f21a21cc99007da9f8a9
Mrunnmayee-13A <mrunmayee13a@gmail.com>
2023-03-30 20:57:21 +0530

    Print Reverse Array Example

---
+++ src/main/java/com/bootcoding/java/b106/assignment/ReverseArray.java
@@ -0,0 +1,11 @@
+package com.bootcoding.java.b106.assignment;
+
+public class ReverseArray {
+    public static void main(String[] args) {
+        int [] ar ={10, 20, 30, 40, 50};
+        for (int i=4; i>=0; i--){
+            System.out.println(ar[i]);
+        }
+
+    }
+}
