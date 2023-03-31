6406d91d50f19d14ef2281073c8f69425f80de0a
Mrunnmayee-13A <mrunmayee13a@gmail.com>
2023-03-31 18:49:25 +0530

    Reverse Array Example

--- src/main/java/com/bootcoding/java/b106/assignment/ReverseArray.java
+++ src/main/java/com/bootcoding/java/b106/assignment/ReverseArray.java
@@ -4,7 +4,7 @@ public class ReverseArray {
     public static void main(String[] args) {
         int [] ar ={10, 20, 30, 40, 50};
         for (int i=4; i>=0; i--){
-            System.out.println(ar[i]);
+            System.out.print(ar[i]+" ");
         }
 
     }
