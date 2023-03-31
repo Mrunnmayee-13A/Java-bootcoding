f0d6ce16e1f7ff8a6677d4b21a08dbb078db995c
Mrunnmayee-13A <mrunmayee13a@gmail.com>
2023-03-31 18:56:12 +0530

    Odd elements sum Example2

---
+++ src/main/java/com/bootcoding/java/b106/assignment/SumofOddNumber.java
@@ -0,0 +1,14 @@
+package com.bootcoding.java.b106.assignment;
+
+public class SumofOddNumber {
+    public static void main(String[] args) {
+        int[] a = {7, 22, 19, 44, 45, 65, 66};
+        int sum = 0;
+        for (int i=0; i<a.length; i++){
+            if (a[i]%2!=0){
+                sum = sum+a[i];
+            }
+        }
+        System.out.println("Sum of all the odd elements is ="+sum);
+    }
+}
