4d814f5ac2b37160d57a8e541e09271eefa9fafd
Mrunnmayee-13A <mrunmayee13a@gmail.com>
2023-03-31 18:48:59 +0530

    Porint Array Example

---
+++ src/main/java/com/bootcoding/java/b106/assignment/PrintArray.java
@@ -0,0 +1,10 @@
+package com.bootcoding.java.b106.assignment;
+
+public class PrintArray {
+    public static void main(String[] args) {
+        int[] a ={10, 20, 30, 40, 50};
+        for (int i=0; i<a.length; i++){
+            System.out.println("Array element = " +a[i]);
+        }
+    }
+}
