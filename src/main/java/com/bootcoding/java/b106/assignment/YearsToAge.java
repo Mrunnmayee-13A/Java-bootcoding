1b1857fa9dc3306bab87453814de77b9f1fc7d29
Mrunnmayee-13A <mrunmayee13a@gmail.com>
2023-04-03 18:48:45 +0530

    Years to age Example

---
+++ src/main/java/com/bootcoding/java/b106/assignment/YearsToAge.java
@@ -0,0 +1,10 @@
+package com.bootcoding.java.b106.assignment;
+
+public class YearsToAge {
+    public static void main(String[] args) {
+        System.out.println(conversionToDys(13));
+    }
+    public static int conversionToDys(int a){
+     return a*365;
+    }
+}
