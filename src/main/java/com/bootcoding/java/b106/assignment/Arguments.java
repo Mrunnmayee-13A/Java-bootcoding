d6a055e34bf8ad1a24302bcb1a426a0e21052b9e
Mrunnmayee-13A <mrunmayee13a@gmail.com>
2023-04-03 18:47:00 +0530

    Arguments Example

---
+++ src/main/java/com/bootcoding/java/b106/assignment/Arguments.java
@@ -0,0 +1,13 @@
+package com.bootcoding.java.b106.assignment;
+
+public class Arguments {
+    public static void main(String[] args) {
+        System.out.println(sumOfNum(5, 8));
+    }
+
+    public static boolean sumOfNum(int a, int b) {
+
+        return a == 10 || b == 10 || (a + b) == 10;
+    }
+}
+
---
+++ src/main/java/com/bootcoding/java/b106/assignment/SumOfArrayElements.java
@@ -0,0 +1,13 @@
+package com.bootcoding.java.b106.assignment;
+
+public class SumOfArrayElements {
+    public static void main(String[] args) {
+        int[] ar={7, 22, 19, 44, 45, 65, 66};
+        int sum=0;
+        for(int i=0; i<ar.length; i++){
+            sum = sum + ar[i];
+        }
+        System.out.println("Sum of the array elements = " +sum);
+
+    }
+}
