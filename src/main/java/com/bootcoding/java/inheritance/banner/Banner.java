2f64f8267232b6c4b22c59c899b64665f44a5d54
Mrunnmayee-13A <mrunmayee13a@gmail.com>
2023-05-04 00:38:57 +0530

    Banner App

---
+++ src/main/java/com/bootcoding/java/inheritance/banner/Banner.java
@@ -0,0 +1,19 @@
+package com.bootcoding.java.inheritance.banner;
+
+public class Banner {
+    String matter;
+    String type;
+    Banner(){
+        System.out.println("Banner object is created");
+    }
+    public void create(){
+
+    }
+    public void display(){
+        System.out.println(matter);
+        System.out.println(type);
+
+    }
+
+
+}
