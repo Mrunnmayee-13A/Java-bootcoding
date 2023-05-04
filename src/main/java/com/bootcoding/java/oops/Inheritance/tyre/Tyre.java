34dfb30abb270aff71184f9329f067bac5b008b6
Mrunnmayee-13A <mrunmayee13a@gmail.com>
2023-05-04 23:17:10 +0530

    Tyre

---
+++ src/main/java/com/bootcoding/java/oops/Inheritance/tyre/Tyre.java
@@ -0,0 +1,13 @@
+package com.bootcoding.java.oops.Inheritance.tyre;
+
+public class Tyre {
+    String Brand;
+    String Type;
+    String color;
+
+    public void Print() {
+        System.out.println(Brand);
+        System.out.println(Type);
+        System.out.println(color);
+    }
+}
