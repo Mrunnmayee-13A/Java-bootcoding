7fcfedda7bfdc413818e9384890c33cd8e050b10
Mrunnmayee-13A <mrunmayee13a@gmail.com>
2023-05-04 23:19:26 +0530

    Meter.java

---
+++ src/main/java/com/bootcoding/java/inheritance/meter/Meter.java
@@ -0,0 +1,14 @@
+package com.bootcoding.java.inheritance.meter;
+
+public class Meter {
+    String type;
+    double voltage;
+    public void create(){
+
+    }
+    public void print(){
+        System.out.println(type);
+        System.out.println(voltage);
+    }
+
+}
