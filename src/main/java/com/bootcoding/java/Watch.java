29648135bcfd6bbe2bdc5af63f3b667037ced547
Mrunnmayee-13A <mrunmayee13a@gmail.com>
2023-05-04 23:18:48 +0530

    Watch.java

---
+++ src/main/java/com/bootcoding/java/Watch.java
@@ -0,0 +1,18 @@
+package com.bootcoding.java;
+
+public class Watch {
+    String brand;
+    String color;
+    double price;
+    String type;
+    String dial;
+    String beltType;
+
+    public static void main(String[] args) {
+        Watch w1 = new Watch();
+        Watch w2 = new Watch();
+    }
+
+    }
+
+
