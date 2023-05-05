ee9db326aa0f01d553077d8689c72fe8baf1b7f1
Mrunnmayee-13A <mrunmayee13a@gmail.com>
2023-05-05 22:24:33 +0530

    CopyElement

--- src/main/java/com/bootcoding/java/b106/assignment/CopyElement.java
+++ src/main/java/com/bootcoding/java/b106/assignment/CopyElement.java
@@ -4,10 +4,12 @@ public class CopyElement {
     public static void main(String[] args) {
         int a[] = {7, 22, 19, 44, 45, 22, 17};
         int b[] = new int[a.length];
+
         for (int i = 0; i < a.length; i++) {
             b[i] = a[i];
         }
         System.out.println("Elements copied are -");
+
         for (int i=0; i<b.length; i++){
             System.out.println(b[i]+ " ");
         }
