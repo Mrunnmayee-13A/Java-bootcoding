952810fea79410d210c5789a430a696be1accdb5
Mrunnmayee-13A <mrunmayee13a@gmail.com>
2023-05-06 23:24:04 +0530

    BannerApp using inheritance

--- src/main/java/com/bootcoding/java/inheritance/banner/BannerApp.java
+++ src/main/java/com/bootcoding/java/inheritance/banner/BannerApp.java
@@ -2,7 +2,9 @@ package com.bootcoding.java.inheritance.banner;
 
 public class BannerApp {
     public static void main(String[] args) {
+
         FlexBanner fb = new FlexBanner();
+
         fb.matter =" New batches of java Boot-coding is starting soon";
         fb.type = " Flex Banner";
         fb.create();
