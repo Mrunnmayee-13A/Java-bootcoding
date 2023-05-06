bf5d2fdc0885f0ff1832b6703c6084e5a09010dc
Mrunnmayee-13A <mrunmayee13a@gmail.com>
2023-05-06 23:17:00 +0530

    ReduceNumberToZero

--- src/main/java/com/bootcoding/java/b106/assignment/ReduceNumberToZero.java
+++ src/main/java/com/bootcoding/java/b106/assignment/ReduceNumberToZero.java
@@ -4,6 +4,7 @@ public class ReduceNumberToZero {
     public static void main(String[] args) {
         int num=14;
         int count=0;
+
           while(num!=0){
             if(num%2==0){
                 num=num/2;
