00c0f00cff2bb8c8854ec4df949e15c09b6fac1a
Mrunnmayee-13A <mrunmayee13a@gmail.com>
2023-05-06 23:14:27 +0530

    ReverseArray

--- src/main/java/com/bootcoding/java/b106/assignment/ReverseArray.java
+++ src/main/java/com/bootcoding/java/b106/assignment/ReverseArray.java
@@ -3,9 +3,15 @@ package com.bootcoding.java.b106.assignment;
 public class ReverseArray {
     public static void main(String[] args) {
         int [] ar ={10, 20, 30, 40, 50};
-        for (int i=4; i>=0; i--){
-            System.out.print(ar[i]+" ");
-        }
+        int temp =0;
+        for ( int i = 0; i<ar.length; i++){
+          temp= ar[i];
+          ar[i]=ar[ar.length-1];
+            ar[ar.length-1]=temp;
 
+        }
+        for(int j=0; j<ar.length; j++)
+        System.out.print(ar[j]+" ");
+        // swapping
     }
 }
