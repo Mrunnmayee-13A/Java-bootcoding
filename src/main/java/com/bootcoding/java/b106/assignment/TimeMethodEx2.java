4757d9561fc840958b564802ba7626d8452e028b
Mrunnmayee-13A <mrunmayee13a@gmail.com>
2023-05-06 23:20:14 +0530

    TimeMethodEx2

--- src/main/java/com/bootcoding/java/b106/assignment/TimeMethodEx2.java
+++ src/main/java/com/bootcoding/java/b106/assignment/TimeMethodEx2.java
@@ -3,10 +3,10 @@ package com.bootcoding.java.b106.assignment;
 public class TimeMethodEx2 {
         public static void main(String[] args) {
             int n= 2;
-            int con = MintoSec(n);
+            int con = MinintoSec(n);
             System.out.println("2 min = "+con+" sec");
         }
-        public static int MintoSec(int n){
+        public static int MinintoSec(int n){
             int sec= n*60;
             return sec;
         }
