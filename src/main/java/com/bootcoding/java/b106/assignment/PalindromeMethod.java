854d4b898e5c36fcce20c7fcba1c60780fd78e34
Mrunnmayee-13A <mrunmayee13a@gmail.com>
2023-05-09 19:21:44 +0530

    Palindrome using Method

--- src/main/java/com/bootcoding/java/b106/assignment/PalindromeMethod.java
+++ src/main/java/com/bootcoding/java/b106/assignment/PalindromeMethod.java
@@ -2,27 +2,28 @@ package com.bootcoding.java.b106.assignment;
 
 public class PalindromeMethod {
     public static void main(String[] args) {
-        int a[] = {10, 121, 191, -34, 20, 0, 111};
+        int[] a = {10, 121, 191, -34, 20, 0, 111};
         int r = 0;
         r = PalindromeNum(a);
         System.out.println("Palindrome numbers are - " + r);
     }
-
     public static int PalindromeNum(int a[]) {
-        int rem = 0;
-        for (int i = 0; i < a.length; i++) {
-            int temp = a[i];
-            while (temp != 0) {
-                rem = rem * 10 + temp % 10;
-                temp = temp / 10;
+        int rem =0;
+        for(int i=0; i<a.length-1; i++){
+            int num= a[i];
+            while(num!=0){
+                rem=rem*10+num%10;
+                num=num/10;
             }
-            if (temp == a[i]) {
-            }
-        }
+            if(rem==a[i]){
 
-return rem;
+            }
+
+        }
+        return rem;
     }
-    }
+}
+
 
 
 
