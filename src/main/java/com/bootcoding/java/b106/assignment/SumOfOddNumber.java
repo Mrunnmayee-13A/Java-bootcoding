2fb4584041baed9a40e829a243d3ded657cc0759
Mrunnmayee-13A <mrunmayee13a@gmail.com>
2023-05-09 19:26:41 +0530

    Sum of OddElements in Array

--- src/main/java/com/bootcoding/java/b106/assignment/SumOfOddNumber.java
+++ src/main/java/com/bootcoding/java/b106/assignment/SumOfOddNumber.java
@@ -1,9 +1,10 @@
 package com.bootcoding.java.b106.assignment;
 
-public class SumofOddNumber {
+public class SumOfOddNumber {
     public static void main(String[] args) {
         int[] a = {7, 22, 19, 44, 45, 65, 66};
         int sum = 0;
+
         for (int i=0; i<a.length; i++){
             if (a[i]%2!=0){
                 sum = sum+a[i];
