6de7e21477545ec84c161f4ee7f4a766dba1cc6f
Mrunnmayee-13A <mrunmayee13a@gmail.com>
2023-05-09 19:23:52 +0530

    Same Elements in Array

--- src/main/java/com/bootcoding/java/b106/assignment/SameElement.java
+++ src/main/java/com/bootcoding/java/b106/assignment/SameElement.java
@@ -3,7 +3,9 @@ package com.bootcoding.java.b106.assignment;
 public class SameElement {
     public static void main(String[] args) {
         int[] ar= {7, 22, 19, 44, 45, 65, 7};
+
             if(ar[0] == ar[ar.length-1]){
+
                 for (int i=0; i<ar.length; i++){
                     System.out.println(ar[i]);
                 }
