4de4d674efb071902f7824d7547e5c01a281e384
Mrunnmayee-13A <mrunmayee13a@gmail.com>
2023-05-09 19:26:16 +0530

    Sum of EvenElements in Array

--- src/main/java/com/bootcoding/java/b106/assignment/SumOfEvenElements.java
+++ src/main/java/com/bootcoding/java/b106/assignment/SumOfEvenElements.java
@@ -4,6 +4,7 @@ public class SumOfEvenElements {
     public static void main(String[] args) {
         int [] a={7, 22, 19, 44, 45, 65, 66};
         int sum=0;
+
          for (int i=0; i<a.length; i++){
              if (a[i]%2==0){
                  sum = sum + a[i];
