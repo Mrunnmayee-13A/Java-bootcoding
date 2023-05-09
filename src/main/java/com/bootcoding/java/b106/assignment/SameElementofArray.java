a4730a9ec7472d1c64f31c6ca256b729260be5ae
Mrunnmayee-13A <mrunmayee13a@gmail.com>
2023-05-09 19:25:15 +0530

    Same Elements of Array

--- src/main/java/com/bootcoding/java/b106/assignment/SameElementofArray.java
+++ src/main/java/com/bootcoding/java/b106/assignment/SameElementofArray.java
@@ -5,12 +5,14 @@ import javax.naming.PartialResultException;
 public class SameElementofArray {
     public static void main(String[] args) {
         int[] ar = {7, 22, 19, 44, 45, 21, 17};
+
        if(ar[1]==ar[ar.length-2]){
            for(int i=0; i<ar.length; i++){
                    System.out.println(ar[i]);
                }
-           }else{
+           }
+       else{
            System.out.println("Elements are not same at respective position.");
        }
-       }
-    }
+     }
+   }
